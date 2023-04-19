6a03df12debb7012613d4ca825ac65419c86da07
dengliangdong <1615527807@qq.com>
2023-04-19 21:38:35 +0800

    init

--- rg/src/main/java/com/example/rg/RgApplication.java
+++ rg/src/main/java/com/example/rg/RgApplication.java
@@ -3,11 +3,15 @@ package com.example.rg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class RgApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RgApplication.class, args);
+		log.info("项目启动成功");
 	}
 
 }
