5e8f429ed69e74153fb15418e3d5860f46cd994d
dengliangdong <1615527807@qq.com>
2023-04-23 23:10:29 +0800

    分页

---
+++ rg/src/main/java/com/example/rg/config/MybatisPlusConfig.java
@@ -0,0 +1,19 @@
+package com.example.rg.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+
+@Configuration
+public class MybatisPlusConfig {
+
+  @Bean
+  public MybatisPlusInterceptor mybatisPlusInterceptor() {
+    MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+    mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+    return mybatisPlusInterceptor;
+  }
+}
--- rg/src/main/java/com/example/rg/controller/EmployeeController.java
+++ rg/src/main/java/com/example/rg/controller/EmployeeController.java
@@ -6,9 +6,11 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.rg.service.EmployeeService;
@@ -16,7 +18,9 @@ import com.example.rg.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 
 import com.example.rg.entity.Employee;
+import com.alibaba.druid.util.StringUtils;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.rg.common.R;;
 
 @Slf4j
@@ -34,14 +38,14 @@ public class EmployeeController {
     LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
     queryWrapper.eq(Employee::getUsername, employee.getUsername());
     Employee emp = employeeService.getOne(queryWrapper);
-    if(emp == null) {
+    if (emp == null) {
       return R.error("登录失败");
     }
-    if(!emp.getPassword().equals(password)){
+    if (!emp.getPassword().equals(password)) {
       return R.error("登录失败");
     }
 
-    if(emp.getStatus() == 0){
+    if (emp.getStatus() == 0) {
       return R.error("账号已禁用");
     }
 
@@ -52,31 +56,44 @@ public class EmployeeController {
 
   /**
    * 员工退出
+   * 
    * @param request
    * @return
    */
   @PostMapping("/logout")
-  public R<String> logout (HttpServletRequest request){
+  public R<String> logout(HttpServletRequest request) {
     request.getSession().removeAttribute("employee");
     return R.success("退出成功");
   }
 
   @PostMapping
-  public R<String> save(HttpServletRequest request,@RequestBody Employee employee) {
+  public R<String> save(HttpServletRequest request, @RequestBody Employee employee) {
     employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
 
     employee.setCreateTime(LocalDateTime.now());
     employee.setUpdateTime(LocalDateTime.now());
 
-    Long empId = (Long)request.getSession().getAttribute("employee");
+    Long empId = (Long) request.getSession().getAttribute("employee");
     employee.setCreateUser(empId);
     employee.setUpdateUser(empId);
-    
+
     log.info("新增员工: {}", employee.toString());
     employeeService.save(employee);
     return R.success("新增员工成功");
-  } 
+  }
 
+  @GetMapping("/page")
+  public R<Page<Employee>> page(@RequestParam int page, @RequestParam int pageSize, @RequestParam(required=false) String name) {
+    log.info("page={}, pageSize={}, name={}", page, pageSize, name);
+    // 分页器
+    Page<Employee> pageInfo = new Page<Employee>(page, pageSize);
+    // 条件查询器
+    LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<Employee>();
+    queryWrapper.like(!StringUtils.isEmpty(name), Employee::getName, name);
+    queryWrapper.orderByDesc(Employee::getUpdateTime);
+    employeeService.page(pageInfo, queryWrapper);
 
+    return R.success(pageInfo);
+  }
 
 }
